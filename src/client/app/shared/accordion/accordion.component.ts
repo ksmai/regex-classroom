82db38f69d6572f4fa8b9126d773028b4e00af71
ksmai <kasingmai@gmail.com>
2017-06-27 23:27:43 +0800

    Implement AccordionComponent

--- src/client/app/shared/accordion/accordion.component.ts
+++ src/client/app/shared/accordion/accordion.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostBinding,
+  Input,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 're-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss'],
 })
-export class AccordionComponent {
+export class AccordionComponent implements AfterViewInit {
+  @HostBinding('class.accordion-open') opened = true;
   @ViewChild('body') private body: any;
   private height: number;
 
+  ngAfterViewInit(): void {
+    this.height = this.body.nativeElement.offsetHeight;
+    this.body.nativeElement.style.height = `${this.height}px`;
+  }
+
   toggleOpen(): void {
+    this.opened = !this.opened;
+    const targetHeight = this.opened ? this.height : 0;
+    this.body.nativeElement.style.height = `${targetHeight}px`;
   }
 }
