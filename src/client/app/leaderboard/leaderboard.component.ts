d6cd6333e48f8cf3a3536430596747e36c69d97c
ksmai <kasingmai@gmail.com>
2017-07-04 22:43:51 +0800

    Fix styles in leaderboard

--- src/client/app/leaderboard/leaderboard.component.ts
+++ src/client/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/pluck';
+import { Observable } from 'rxjs/Observable';
+
+import { IUser, UserService } from '../core/user.service';
 
 /**
  * Renders the leaderboard showing the users with highest level and
@@ -8,5 +13,18 @@ import { Component } from '@angular/core';
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss'],
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+  userList: Observable<IUser[]>;
+  me: IUser;
+
+  constructor(
+    private routes: ActivatedRoute,
+    private userService: UserService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.userList = this.routes.data.pluck('users');
+    this.userService.getUser().subscribe((user) => this.me = user);
+  }
 }
